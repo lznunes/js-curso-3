b8a0cfc5e9a8bb9ab23f121534704defcda00fff
Luiz Antonio Nunes Neves <lznunes@gmail.com>
2024-01-29 10:25:53 -0300

    aleert e title

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+function verificarChute(){
+    console.log('O botão foi clicado!')
+}
+
+function alertMessage(){
+    alert('Eu amo JS')
+}
